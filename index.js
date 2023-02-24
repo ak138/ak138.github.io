ff83f4bb517ec4b98f5190d95afa7349199e7936
Alexandra Karina Petkova <103148963+ak138@users.noreply.github.com>
2023-02-24 16:03:28 -0500

    Add files via upload
    initial commit

---
+++ index.js
@@ -0,0 +1,26 @@
+document.getElementById("summitButton").onclick=function(){
+    let temp;
+    if(document.getElementById("cButton").checked){
+        temp=document.getElementById("textBox").value;
+        temp=Number(temp);
+        temp=toCelsius(temp);
+        document.getElementById("tempLabel").innerHTML=temp+ "°C";
+
+    }
+    else if(document.getElementById("fButton").checked){
+        temp=document.getElementById("textBox").value;
+        temp=Number(temp);
+        temp=toFahrenheit(temp);
+        document.getElementById("tempLabel").innerHTML=temp+ "°F ";
+
+    }
+    else{
+        document.getElementById("tempLabel").innerHTML="select a unit";
+    }
+}
+function toCelsius(temp){
+    return(temp-32)*(5/9);
+}
+function toFahrenheit(temp){
+    return temp *9/5 +32;
+}
